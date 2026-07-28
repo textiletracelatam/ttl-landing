dbb076f2cf6f650ba3656c6adc89e96d3c0a392f
Santiago Munera <sf.munera1130@gmail.com>
2026-07-28 02:45:28 -0500

    Formularios de contacto y aliados listos

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const appConfig = {
   email: "contacto@textiletracelatam.com",
   location: "Bogotá, Colombia",
   logo: "https://d1yei2z3i6k35z.cloudfront.net/15861133/69715e5480fa7_logonegrosinfondo.png",
-
+  ttlApiHost: import.meta.env.PUBLIC_TTL_API_HOST || "http://localhost:9090",
   footerLinks: [
     { name: "Nosotros", href: "/nosotros" },
     { name: "Blog", href: "/blog" },
